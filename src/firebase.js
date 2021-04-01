a803ddc9dff4a68fbf278221c96f529ac7adbccc
Lukasz Markowski <lucas20193@gmail.com>
2021-04-01 14:00:55 +0200

    folder setup and add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_pgeFWD8dUkn_jzj8M4ZIwADhFe4YYOg",
+  authDomain: "netflix-clone-362d0.firebaseapp.com",
+  projectId: "netflix-clone-362d0",
+  storageBucket: "netflix-clone-362d0.appspot.com",
+  messagingSenderId: "330964819363",
+  appId: "1:330964819363:web:7b8962e47af5cb3b648e16",
+  measurementId: "G-FRHM8BZRTS"
+};
